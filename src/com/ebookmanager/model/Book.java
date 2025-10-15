f2f5b5c4cbe7067daec06be78e2274a7d8ee06db
KTT85 <trankien8805@gmail.com>
2025-10-15 08:54:27 +0700

    The first add for

--- src/com/ebookmanager/model/Book.java
+++ src/com/ebookmanager/model/Book.java
@@ -1,5 +1,66 @@
 package com.ebookmanager.model;
 
 public class Book {
-    
+    private int bookId;
+    private String bookTitle;
+    private String authorName;
+    private String coverImage;
+    private String filePath;
+    private String publishDate;
+    public Book() {
+    }
+    public Book(int bookId, String bookTitle, String authorName, String coverImage, String filePath, String publishDate) {
+        this.bookId = bookId;
+        this.bookTitle = bookTitle;
+        this.authorName = authorName;
+        this.coverImage = coverImage;
+        this.filePath = filePath;
+        this.publishDate = publishDate;
+    }
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+    public String getCoverImage() {
+        return coverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
 }
