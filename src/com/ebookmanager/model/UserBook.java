32bd613c1537fbc5e9a913c0643d226379e38889
Huy Cao <caongochuypega@gmail.com>
2025-10-20 10:59:48 +0700

    UserBook changes

--- src/com/ebookmanager/model/UserBook.java
+++ src/com/ebookmanager/model/UserBook.java
@@ -1,17 +1,21 @@
 package com.ebookmanager.model;
 import java.util.*;
 import com.ebookmanager.dao.*;
-
+import com.ebookmanager.dao.UserBookDAO;
 public class UserBook {
     private ArrayList<Book> books;
     private ArrayList<Highlight> highlights;
-    private String user_id;
-    private String book_id;
+    private int user_id;
+    private int book_id;
     private String date_added;
+    // Reading progress of each book in user list;
     private float reading_progress;
     
+    // Declare a userbookdao object for userbook of user.
+    UserBookDAO userbookdao = new UserBookDAO();
 
-    public UserBook(String user_id, String date_added)
+    // Getters
+    public UserBook(Integer user_id, String date_added)
     {
         this.user_id = user_id;
         this.date_added = date_added;
@@ -22,39 +26,52 @@ public class UserBook {
     public ArrayList<Highlight> getHighlights() {
         return highlights;
     }
-    public String getUser_id() {
+    public int getUser_id() {
         return user_id;
     }
-    public String getBook_id() {
+    public int getBook_id() {
         return book_id;
     }
     public String getDate_added() {
         return date_added;
     }
-    public float getReading_progress(String user_id) {
-        return reading_progress;
-    }
-
+//--------------------------------------------------------------------//
     // Setters
 
+    // Collect lasted reading progress of book in user list
+    public float getReading_progress(Book book) {
+        return userbookdao.getProgress(user_id, book);
+    }
+
+
+    // ADD BOOK INTO USERBOOK
+    public void addBook(Book book)
+    {
+        books.add(book);
+        userbookdao.addBook(book, user_id);
+    }
+
+    // SAVE BOOK'S PROGRESS IN USER LIST
     public void saveProgress(User user, Book book, float progress)
     {
         /*
-         * This is place for a function which take progress from book file
+         * This is place for a function which take indexes
+         * in reading screen
          */
-        takeProgress tp = new takeProgress();
-        progress = tp.collectProgress(book);
-        saveProgressDAO spD = new saveProgressDAO();
-        spD.saveProgress(user, book, progress);
+        userbookdao.saveProgress(user, book, progress);
     }
+
+    // ADD HIGHLIGHT INTO BOOK'S PAGE AND 
+    // ADD HIGHLIGHT'S INFORMATION INTO DATABASE
     public void addHighlight(User user, Book book)
     {
         // Take index from book file
         // when user make highlight on screen
-        takeProgress tp = new takeProgress();
-        int start = tp.takeFirstPos(book);
-        int end = tp.takeLastPos(book);
 
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        sc.close();
         // save index into new highlight object
         Highlight highlight = new Highlight();
         highlight.setStartPos(start);
